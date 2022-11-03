cd948e2966604fa6dfd5e8d23efdf73766c789a5
anil843324 <anilkrsingh14200@gmail.com>
2022-11-03 12:45:07 +0530

    getProductReviews and deleteReviews done

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,6 +6,8 @@ const {
   deleteProduct,
   getProductDetails,
   createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require('../controllers/productController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
@@ -23,9 +25,11 @@ router
 
 router.route('/product/:id').get(getProductDetails);
 
+router.route('/review').put(isAuthenticatedUser, createProductReview);
 
-router.route('/review').put( isAuthenticatedUser , createProductReview )
-
-
+router
+  .route('/reviews')
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
